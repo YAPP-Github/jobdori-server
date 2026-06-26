7c6851e4768482a56d2f59828b29d721f812aebc
seungh0 <will.seungho@gmail.com>
2026-06-27 03:57:22 +0900

    [NO-ISSUE] 헤더 관련 문서 보강

--- jobdori-api/src/main/kotlin/com/jobdori/api/application/auth/dto/request/LoginRequest.kt
+++ jobdori-api/src/main/kotlin/com/jobdori/api/application/auth/dto/request/LoginRequest.kt
@@ -3,22 +3,26 @@ package com.jobdori.api.application.auth.dto.request
 import com.jobdori.core.application.auth.command.AuthCommand
 import com.jobdori.core.domain.user.UserIdentityProvider
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+import org.hibernate.validator.constraints.URL
 
 data class LoginRequest(
     val provider: UserIdentityProvider,
 
+    @field:Size(max = 100)
     @field:NotBlank
     val authorizationCode: String = "",
 
+    @field:Size(max = 300)
+    @field:URL
     @field:NotBlank
     val redirectUri: String = "",
 ) {
 
-    fun toCommand(): AuthCommand =
-        AuthCommand(
-            provider = provider,
-            authorizationCode = authorizationCode,
-            redirectUri = redirectUri,
-        )
+    fun toCommand(): AuthCommand = AuthCommand(
+        provider = provider,
+        authorizationCode = authorizationCode,
+        redirectUri = redirectUri,
+    )
 
 }

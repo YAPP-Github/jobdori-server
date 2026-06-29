ce0610b753753eca7b727e21f7019e3fc712f2fd
seungh0 <will.seungho@gmail.com>
2026-06-30 01:49:36 +0900

    [NO-ISSUE] 샘플 코드 제거

--- jobdori-core/src/main/kotlin/com/jobdori/core/application/sample/service/UpdateSampleService.kt
+++
@@ -1,8 +0,0 @@
-package com.jobdori.core.application.sample.service
-
-import com.jobdori.core.application.sample.service.command.UpdateSampleCommand
-import com.jobdori.core.application.sample.service.result.SampleResult
-
-interface UpdateSampleService {
-    fun update(command: UpdateSampleCommand): SampleResult
-}
--- jobdori-core/src/main/kotlin/com/jobdori/core/application/sample/service/result/SampleResult.kt
+++
@@ -1,7 +0,0 @@
-package com.jobdori.core.application.sample.service.result
-
-data class SampleResult (
-    val sampleId: Long,
-    val name: String,
-)
-

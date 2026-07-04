21b232812b1bbbdaeef6781e7f1a27e6df953c4b
seungh0 <will.seungho@gmail.com>
2026-07-04 16:33:00 +0900

    [ISSUE-26] 경험 검색시 제목 이외에 내용도 포함되도록 수정

--- jobdori-infrastructure/persistence/src/main/kotlin/com/jobdori/infrastructure/persistence/domain/experience/repository/ExperienceCustomRepositoryImpl.kt
+++ jobdori-infrastructure/persistence/src/main/kotlin/com/jobdori/infrastructure/persistence/domain/experience/repository/ExperienceCustomRepositoryImpl.kt
@@ -31,13 +31,19 @@ class ExperienceCustomRepositoryImpl(
                     and(
                         path(ExperienceEntity::workspaceId).eq(workspaceId),
                         path(ExperienceEntity::status).eq(status),
-                        lower(path(ExperienceEntity::title)).like(keywordPattern, '\\'),
+                        or(
+                            lower(path(ExperienceEntity::title)).like(keywordPattern, '\\'),
+                            lower(path(ExperienceEntity::contents)).like(keywordPattern, '\\'),
+                        ),
                     )
                 } else {
                     and(
                         path(ExperienceEntity::workspaceId).eq(workspaceId),
                         path(ExperienceEntity::status).eq(status),
-                        lower(path(ExperienceEntity::title)).like(keywordPattern, '\\'),
+                        or(
+                            lower(path(ExperienceEntity::title)).like(keywordPattern, '\\'),
+                            lower(path(ExperienceEntity::contents)).like(keywordPattern, '\\'),
+                        ),
                         path(ExperienceEntity::id).lessThan(cursorId),
                     )
                 },

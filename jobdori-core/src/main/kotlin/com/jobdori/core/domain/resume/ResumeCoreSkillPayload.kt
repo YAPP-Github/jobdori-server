82dd64dfb0c8edf425af6569a5cbbcd97b5c9763
seungh0 <will.seungho@gmail.com>
2026-07-25 03:15:02 +0900

    [NO-ISSUE] isInitialItem=true로 기본 생성하도록 수정

--- jobdori-core/src/main/kotlin/com/jobdori/core/domain/resume/ResumeCoreSkillPayload.kt
+++ jobdori-core/src/main/kotlin/com/jobdori/core/domain/resume/ResumeCoreSkillPayload.kt
@@ -2,7 +2,7 @@ package com.jobdori.core.domain.resume
 
 data class ResumeCoreSkillPayload(
     val content: String?,
-    val isInitialItem: Boolean = false,
+    val isInitialItem: Boolean = true,
 ) : ResumeSectionItemPayload {
     override val type = ResumeSectionType.CORE_SKILL
 }

17ddfb0c18f1645f45242dd7eb9fcc7d561e28fb
seungh0 <will.seungho@gmail.com>
2026-08-01 19:24:42 +0900

    [NO-ISUSE] 에러 메시지 수정

--- jobdori-core/src/main/kotlin/com/jobdori/core/domain/resume/error/ResumeErrorCode.kt
+++ jobdori-core/src/main/kotlin/com/jobdori/core/domain/resume/error/ResumeErrorCode.kt
@@ -12,7 +12,7 @@ enum class ResumeErrorCode(
     E400_RESUME_SECTION_ITEM_REQUIRED(
         httpStatusCode = 400,
         code = "resume_section_item_required",
-        message = "섹션에는 최소 하나 이상의 item이 필요합니다.",
+        message = "빈 섹션이 있어 저장할 수 없어요. 항목을 추가하거나 빈 섹션을 지워 주세요.",
         description = "기본 아이템을 사용하지 않는 이력서 섹션에 아이템이 없는 경우",
     ),
     E404_RESUME_NOT_FOUND(

5bd9fac2b61d392767b46161c36b0824a33ccf6c
seungh0 <will.seungho@gmail.com>
2026-08-15 15:28:30 +0900

    [NO-ISSUE] STAR 경험 비어있는거 허용하도록 수정

--- jobdori-api/src/main/kotlin/com/jobdori/api/application/experience/dto/request/contents/StarExperienceContentsRequest.kt
+++ jobdori-api/src/main/kotlin/com/jobdori/api/application/experience/dto/request/contents/StarExperienceContentsRequest.kt
@@ -2,23 +2,18 @@ package com.jobdori.api.application.experience.dto.request.contents
 
 import com.fasterxml.jackson.annotation.JsonIgnore
 import com.jobdori.core.domain.experience.StarExperienceContents
-import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Size
 
 data class StarExperienceContentsRequest(
-    @field:NotBlank(message = "상황을 입력해 주세요.")
     @field:Size(max = 500, message = "입력 가능한 상황의 최대 길이는 {max}자입니다.")
     val situation: String = "",
 
-    @field:NotBlank(message = "과제를 입력해 주세요.")
     @field:Size(max = 500, message = "입력 가능한 과제의 최대 길이는 {max}자입니다.")
     val task: String = "",
 
-    @field:NotBlank(message = "행동을 입력해 주세요.")
     @field:Size(max = 500, message = "입력 가능한 행동의 최대 길이는 {max}자입니다.")
     val action: String = "",
 
-    @field:NotBlank(message = "결과를 입력해 주세요.")
     @field:Size(max = 500, message = "입력 가능한 결과의 최대 길이는 {max}자입니다.")
     val result: String = "",
 ) {
